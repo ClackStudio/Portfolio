55950abb017b926ffe0ef3215a3e281baa35331e
Theodor Hillmann <theo.hillmann@gmail.com>
2023-05-31 14:21:45 +0200

    hotfix/logo

--- src/components/FullScreenAnimation.js
+++ src/components/FullScreenAnimation.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import logo from '../img/logo_2.svg'
-import { useTransitionStore } from '../stores/TransitionStore'
-import { useSpring, animated } from 'react-spring'
+import React, { useEffect, useState } from "react";
+import logo from "../img/clack-studio_logo.svg";
+import { useTransitionStore } from "../stores/TransitionStore";
+import { useSpring, animated } from "react-spring";
 
 const FullScreenAnimation = ({ data }) => {
-  const { setIntroAnimationDone } = useTransitionStore()
+  const { setIntroAnimationDone } = useTransitionStore();
 
-  const [animated1, setAnimated1] = useState(false)
+  const [animated1, setAnimated1] = useState(false);
 
   const startValues = {
     position: [0, 7, 1],
     args: [20, 1],
-  }
+  };
 
   const endValues = {
     position: [0, -7, 1],
     args: [20, 6],
-  }
+  };
 
   const [spring, springApi] = useSpring(() => ({
     from: { ...startValues },
@@ -24,23 +24,23 @@ const FullScreenAnimation = ({ data }) => {
     onRest: () => setIntroAnimationDone(),
     config: { mass: 1, tension: 2, friction: 0, clamp: true },
     delay: 0,
-  }))
+  }));
 
-  springApi.start()
+  springApi.start();
 
   useEffect(() => {
     setTimeout(() => {
-      setAnimated1(true)
-    }, 400)
-  }, [animated1])
+      setAnimated1(true);
+    }, 400);
+  }, [animated1]);
 
   return (
-    <animated.div className={'intro-logo-wrapper'}>
-      <div className={`intro-logo-wrapper ${animated1 ? 'transparent' : ''}`}>
+    <animated.div className={"intro-logo-wrapper"}>
+      <div className={`intro-logo-wrapper ${animated1 ? "transparent" : ""}`}>
         <animated.img src={logo} className="intro-logo"></animated.img>
       </div>
     </animated.div>
-  )
-}
+  );
+};
 
-export default FullScreenAnimation
+export default FullScreenAnimation;
